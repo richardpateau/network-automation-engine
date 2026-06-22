fb08d041098e3a6e4dbc9c2df7c5040ea0401ff7
richardpateau <richardpateau74@gmail.com>
2026-06-22 09:24:39 -0400

    SNMP

--- PNET_19_Automation/Hybrid_Automation.py
+++ PNET_19_Automation/Hybrid_Automation.py
@@ -578,9 +578,9 @@ def build_stp_global(device_state):
 
 		actual_stp["vlan_priorities"][vlan] = priority
 	return actual_stp
-def build_stp_interfaces(parse):
+def build_stp_interfaces(running_config):
+	parse = CiscoConfParse(running_config.splitlines())
 	act_stp_int  = {}
-
 	for interface in parse.find_objects(r"^interface"):
 		name = interface.text.split()[1]
 		act_stp_int[name] = {
@@ -924,7 +924,47 @@ def build_snmp_nc(netconf_state):
 	if location: 
 		actual_snmp["location"] = location
 	return actual_snmp
-def build_snmp_netmiko(device_state): 
+def build_snmp_netmiko(running_config):
+	parse = CiscoConfParse(running_config.splitlines())
+	actual_snmp = {
+			"communities": [],
+	    	"hosts": [],
+	    	"traps": {},
+	    	"location": "",
+	    	"contact": ""
+		}
+
+	for s in parse.find_objects(r"^snmp-server community"):
+		parts = s.text.split()
+
+		actual_snmp["communities"].append({
+				"snmp_name": parts[2],
+				"permission": parts[3].lower()
+			})
+	for s in parse.find_objects(r"^snmp-server location"):
+		parts = s.text.split()
+
+		actual_snmp["location"] = " ".join(parts[2:])
+	for s in parse.find_objects(r"^snmp-server contact"):
+		parts = s.text.split()
+
+		actual_snmp["contact"] = " ".join(parts[2:])
+	for s in parse.find_objects(r"^snmp-server enable traps"):
+		line = s.text.lower()
+
+		if " enable traps snmp" in line: 
+			actual_snmp["traps"]["snmp"] = True 
+		if " enable traps syslog" in line: 
+			actual_snmp["traps"]["syslog"] = True 
+	for s in parse.find_objects(r"^snmp-server host"):
+		parts = s.text.split()
+		if len(parts) >= 6: 
+			actual_snmp["hosts"].append({
+					"snmp_ip": parts[2],
+					"snmp_version": parts[4],
+					"snmp_name": parts[5]
+				})
+	return actual_snmp
 def check_vlan(expected_vlans, actual_vlans):
 	failures = []
 
@@ -1585,6 +1625,97 @@ def check_dhcp(expected_dhcp, actual_config):
 					f"Interface: {interface_name} | Helper IP: {e}"
 				)
 	return len(failures) == 0, failures  
+def check_snmp_nc(expected_snmp, actual_config): 
+	failures = []
+	exp_communities = expected_snmp.get("communities", [])
+	act_communities = actual_config.get("communities", [])
+	exp_tuple = {(e.get("snmp_name")):(e.get("permission")) for e in exp_communities}
+	act_tuple = {(a.get("snmp_name")): (a.get("permission")) for a in act_communities}
+	extra_c = act_tuple.keys() - exp_tuple.keys()
+	for name in extra_c: 
+		failures.append(
+				f"(SNMP) Drift Detected: Unexpected SNMP Community Found | "
+				f"Name: {name} | Permission: {act_tuple.get(name)}"
+			)
+	for name in exp_tuple: 
+		actual = act_tuple.get(name)
+		if actual is None: 
+			failures.append(
+					f"(SNMP) Missing SNMP Community | Name: {name}"
+				)
+			continue
+		exp_permission = exp_tuple.get(name)
+
+		if exp_permission != actual: 
+			failures.append(
+					f"(SNMP) Mismatched Community Permission | Name: {name} | "
+					f"Expected: {exp_permission} | Actual: {actual}"
+				)
+	if expected_snmp.get("contact") != actual_config.get("contact"):
+		failures.append(
+				f"(SNMP) Mismatched Contact | Expected: {expected_snmp.get('contact')} | "
+				f"Actual: {actual_config.get('contact')}"
+			)
+	exp_hosts = normalize_to_list(expected_snmp.get("hosts"))
+	act_hosts = normalize_to_list(actual_config.get("hosts"))
+
+	exp_tuple = {
+			(h.get("snmp_name", ""), h.get("snmp_ip", ""), h.get("snmp_version", ""))
+			for h in exp_hosts
+	}
+	act_tuple = {
+				(h.get("snmp_name", ""), h.get("snmp_ip", ""), h.get("snmp_version", ""))
+				for h in act_hosts
+		}
+	extra = act_tuple - exp_tuple 
+
+	for name, ip, version in extra: 
+		failures.append(
+				f"(SNMP) Extra SNMP Found | Name: {name} | IP: {ip} | "
+				f"Version: {version}"
+			)
+	act_lookup = {(a.get("snmp_name")): a for a in act_hosts}
+	for e in exp_hosts: 
+		actual = act_lookup.get(e.get("snmp_name"))
+		if not actual: 
+			failures.append(
+					f"(SNMP) Community Name Not Found | Name: {e.get('snmp_name')}"
+				)
+			continue
+		if actual.get("snmp_ip") != e.get("snmp_ip"): 
+			failures.append(
+					f"(SNMP) Mismatched SNMP IP Found | Name: {e.get('snmp_name')} | "
+					f"Expected IP: {e.get('snmp_ip')} | Actual: {actual.get('snmp_ip')}"
+				)
+		if actual.get("snmp_version") != e.get("snmp_version"): 
+			failures.append(
+					f"(SNMP) Mismatched SNMP Version | Name: {e.get('snmp_name')} | "
+					f"Expected: {e.get('snmp_version')} | Actual: {actual.get('snmp_version')}"
+				)
+	if expected_snmp.get("location") != actual_config.get("location"): 
+		failures.append(
+				f"(SNMP) SNMP Location Mismath | Expected: {expected_snmp.get('location')} | "
+				f"Actual: {actual_config.get('location')}" 
+			)
+	exp_traps = expected_snmp.get("traps", {})
+	act_traps = actual_config.get("traps", {})
+	if exp_traps.get("config"): 
+		if exp_traps.get("config") != act_traps.get("config"):
+			failures.append(
+					f"(SNMP) Mismatched Traps | Config | Expected: {exp_traps.get('config')} | "
+					f"Actual: {act_traps.get('config')}"
+				)
+	if exp_traps.get('snmp') != act_traps.get('snmp'):
+		failures.append(
+				f"(SNMP) Mismatched Traps | SNMP | Expected: {exp_traps.get('snmp')} | "
+				f"Actual: {act_traps.get('snmp')}"
+			)
+	if exp_traps.get('syslog') != act_traps.get('syslog'): 
+		failures.append(
+				f"(SNMP) Mismatched Traps | Syslog | Expected: {exp_traps.get('syslog')} | "
+				f"Actual: {act_traps.get('syslog')}"
+			)
+	return len(failures) == 0, failures
 def configure_vlan(conn, device_ip, vlan_data, log): 
 	vlan_id = vlan_data.get("vlan_id", "")
 	name = vlan_data.get("name", "")
