51166ea2e249d59253ce8408e7c1cc92ba2a0d42
Your Name <you@example.com>
2026-09-14 08:05:19 -0400

    chore: remove nested project copy

--- Network_Automation/src/core/session.py
+++
@@ -1,5 +0,0 @@
-class SessionContext: 
-	def __init__(self, session, base_url, device_ip): 
-		self.session = session
-		self.base_url = base_url 
-		self.device_ip = device_ip 
--- Network_Automation/src/sot/client.py
+++
@@ -1,9 +0,0 @@
-import pynetbox
-from src.core.settings import NETBOX_URL, NETBOX_TOKEN
-
-
-def get_netbox():
-    return pynetbox.api(
-        url=NETBOX_URL,
-        token=NETBOX_TOKEN,
-    )
